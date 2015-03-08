00e25af56843c94483d6bf99f9c57afa1a9b0819
Lihua Ma <malihua628@hotmail.com>
2015-03-08 14:06:09 -0700

    LoadData

---
+++ LoadData.R
@@ -0,0 +1,8 @@
+fileName <- "household_power_consumption.txt"
+data <- read.table(fileName, 
+                   header = TRUE, 
+                   sep = ";", 
+                   colClasses = c("character", "character", rep("numeric",7)),
+                   na = "?")
+data1 <- data[which(data$Date == "1/2/2007" | data$Date =="2/2/2007"),]
+data1$DateTime <- strptime(paste(data1$Date, data1$Time), "%d/%m/%Y %H:%M:%S")
